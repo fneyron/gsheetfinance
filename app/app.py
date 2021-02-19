d364d08a8575910ec8bb26535ee7aed81102af11
florent.neyron <florent.neyron@gmail.com>
2021-02-19 18:31:41 +0100

    q

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ def holders(option, share):
     tick = yf.Ticker(share)
     df = getattr(tick, option)
     if not df is None:
-        output = make_response(df.to_csv(header=False, index=False))
+        output = make_response(df.to_csv(header=False, index=False, decimal=','))
         output.headers["Content-Disposition"] = "attachment; filename=export.csv"
         output.headers["Content-type"] = "text/csv"
     else: output = "Error with %s function on %s ticker" % (option, share)
