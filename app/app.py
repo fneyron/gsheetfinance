318e240f4ef29f304d897ccb347b5c54d617647b
florent.neyron <florent.neyron@gmail.com>
2021-02-19 18:24:15 +0100

    q

--- app/app.py
+++ app/app.py
@@ -4,57 +4,20 @@ from flask import Flask, request, jsonify, make_response
 app = Flask(__name__)
 
 
-@app.route('/<share>/holders/', methods=['GET'])
-def holders(share):
-    tick = yf.Ticker('aapl')
-    df = tick.major_holders
+@app.route('/yfinance/<option>/<share>/', methods=['GET'])
+def holders(option, share):
 
-    output = make_response(df.to_csv(header=False, index=False))
-    output.headers["Content-Disposition"] = "attachment; filename=export.csv"
-    output.headers["Content-type"] = "text/csv"
+    tick = yf.Ticker(share)
+    df = getattr(tick, option)
+    if not df is None:
+        output = make_response(df.to_csv(header=False, index=False))
+        output.headers["Content-Disposition"] = "attachment; filename=export.csv"
+        output.headers["Content-type"] = "text/csv"
+    else: output = "Error with %s function on %s ticker" % (option, share)
 
     return output
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return "<h1>YFinance-App</h1>" \
+           "<div>Do the same as rapid-api but for free :)</div>"
