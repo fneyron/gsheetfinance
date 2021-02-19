db98e7167b6074bdf4784f008d7647b6361e2066
florent.neyron <florent.neyron@gmail.com>
2021-02-19 19:53:47 +0100

    q

--- app/app.py
+++ app/app.py
@@ -1,11 +1,12 @@
 import yfinance as yf
+from yahoofinancials import YahooFinancials
 import pandas as pd
 from flask import Flask, request, jsonify, make_response
 app = Flask(__name__)
 
 
 @app.route('/yfinance/<option>/<share>/', methods=['GET'])
-def holders(option, share):
+def yfinance(option, share):
 
     tick = yf.Ticker(share)
     df = getattr(tick, option)
@@ -16,6 +17,27 @@ def holders(option, share):
     else: output = "Error with %s function on %s ticker" % (option, share)
 
     return output
+
+@app.route('/yahoofinancials/<share>/', methods=['GET'])
+def yahoofinancials(share):
+
+    yahoo_financials = YahooFinancials(share)
+    tf = request.args.get('timeframe')
+    type = request.args.get('type')
+
+    income_statement_data_qt = yahoo_financials.get_financial_stmts(tf, type)
+    datas = income_statement_data_qt['incomeStatementHistory'][share]
+    datas = [dict(x[y], **{'Date': y}) for x in datas for y in x]
+    df = pd.DataFrame(datas)
+
+    if not df is None:
+        output = make_response(df.to_csv(index=False, decimal=','))
+        output.headers["Content-Disposition"] = "attachment; filename=export.csv"
+        output.headers["Content-type"] = "text/csv"
+    else: output = "Error with %s ticker options : %s" % (share, request.args)
+
+    return output
+
 
 @app.route('/')
 def index():
